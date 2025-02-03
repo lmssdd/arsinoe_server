87b035ebfc1b9b6b5505c1182303e555b55650da
lmssdd <luca.massidda@gmail.com>
2025-02-03 15:49:10 +0100

    first deploy

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def configure():
 
 if __name__ == '__main__':
     configure()
-    uvicorn.run(app, port=8000, host='127.0.0.1')
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    #uvicorn.run(app, port=8000, host='127.0.0.1')
     #uvicorn.run(app, port=80,   host='127.0.0.1')
     #uvicorn.run(app, port=8000, host='156.148.14.177')
 else:
